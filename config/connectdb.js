c44d9b1c4c45ce51adee00fa8cc14544926bfe54
Prashanth Reddy <prashu2530@gmail.com>
2023-10-05 23:38:08 +0530

    first

---
+++ config/connectdb.js
@@ -0,0 +1,26 @@
+// const mongoose=require('mongoose')
+// const colors=require('colors')
+// const connectdb =async()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGO_URL);
+//         console.log(`server running on ${mongoose.connection.host}`.bgCyan.white);
+//     }
+//     catch (error){
+//         console.log('${error}'.bgRed);
+//     }
+// }
+// module.exports=connectdb
+
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+const connectdb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Server running on ${mongoose.connection.host}`.bgCyan.white);
+  } catch (error) {
+    console.log(`${error}`.bgRed); // Use backticks to interpolate the error variable
+  }
+};
+
+module.exports = connectdb;
